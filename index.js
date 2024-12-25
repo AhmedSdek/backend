6550bf67d300bdf01ce78b3bdd467bc160b5bd9d
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2024-12-25 20:20:10 +0200

    2

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// import express from 'express'
-// import mongoose from 'mongoose';
-// import 'dotenv/config';
-// import menuRouter from './src/menu/menuRoute.js';
-// import cors from 'cors';
-// import userRouter from './src/Auth/userRoute.js';
-// import cartRouter from './src/cart/cartRoute.js';
-// import { Server } from "socket.io";
-// const app = express()
-// const port = process.env.PORT || 3000
-// app.use(cors());
-// app.use(express.json());
-// app.use('/user', userRouter)
-// app.use('/cart', cartRouter)
-// app.use('/api/menu', menuRouter)
-// mongoose
-//     .connect(process.env.DB_URL)
-//     .then(() => console.log("Connected!"))
-//     .catch((err) => console.log(err));
-
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })
-
 
 import express from 'express';
 import mongoose from 'mongoose';
@@ -50,7 +26,9 @@ const io = new Server(server, {
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
 // المسارات
 app.use('/user', userRouter);
 app.use('/cart', cartRouter);
@@ -62,20 +40,7 @@ mongoose
     .then(() => console.log("Connected!"))
     .catch((err) => console.log(err));
 
-// Socket.IO Handling
-// io.on("connection", (socket) => {
-//     console.log("A user connected:", socket.id);
-//     // مثال: استقبال رسالة من العميل
-//     socket.on("message", (data) => {
-//         console.log("Message received:", data);
-//         // إرسال رد للعميل
-//         socket.emit("message", "Message received on the server");
-//     });
-//     // عند قطع الاتصال
-//     socket.on("disconnect", () => {
-//         console.log("A user disconnected:", socket.id);
-//     });
-// });
+
 // مشاركة io مع مسار الطلبات
 app.set('socketio', io);
 // بدء تشغيل الخادم
